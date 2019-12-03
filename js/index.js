f858d087cb6a8fe1feece6f2e865ad60cca6a498
agohorel <gohorelalex@gmail.com>
2019-12-03 15:04:32 -0500

    setup selectors and click listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-// Your code goes here
+// nav logo
+const navLogo = document.querySelector(".logo-heading");
+
+navLogo.addEventListener("click", () => {
+  alert("clicked!");
+});
+
+// nav items
+const navItems = document.querySelectorAll(".nav-link");
+
+navItems.forEach(item => {
+  item.addEventListener("click", () => {
+    alert("clicked!");
+  });
+});
+
+// hero img
+const hero = document.querySelector(".intro img");
+
+hero.addEventListener("click", () => {
+  alert("clicked!");
+});
+
+// all other imgs
+const imgs = document.querySelectorAll(
+  ".img-content img, .content-destination img"
+);
+
+imgs.forEach(img => {
+  img.addEventListener("click", () => {
+    alert("clicked!");
+  });
+});
+
+// buttons
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("clicked!");
+  });
+});
